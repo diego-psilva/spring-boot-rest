4279f5288d2c75b12bc5e10d946e7016a71b5729
Diego PS <diegops.tads@gmail.com>
2020-11-11 12:01:09 -0300

    Validation methods

---
+++ src/main/java/com/example/rest/resource/MathController.java
@@ -0,0 +1,37 @@
+package com.example.rest.resource;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MathController {
+
+
+   @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+   public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numeberTwo) throws Exception {
+
+      if (!isNumeric(numberOne) || !isNumeric(numeberTwo)) {
+
+         throw new Exception();
+      }
+
+      Double sum = convertToDouble(numberOne) + convertToDouble(numeberTwo);
+
+      return sum;
+   }
+
+   private Double convertToDouble(String strNumber) {
+      if (strNumber == null) return 0D; // verfica se é nulo
+      String number = strNumber.replaceAll(",", "."); // substitui virgula por ponto
+      if(isNumeric(number)) return Double.parseDouble(number);
+      return 0D;
+   }
+
+   private boolean isNumeric(String strNumber) {
+
+      if (strNumber == null) return false; // verfica se é nulo
+      String number = strNumber.replaceAll(",", "."); // substitui virgula por ponto
+      return number.matches("[-+]?[0-9]*\\.?[0-9]+"); // verifica se é um numero
+   }
+
+
+}
